5dfe9800de8b01f9560145db14577ec832a7e36e
raoul <raouldev@outlook.com>
2025-04-27 16:34:03 -0500

    Fixed input bug after creating or removing modules. Added checking if there are units in the scene or if the units have modules when playing. Added exit function, and make input look nicer

--- src/interface/Interface.cpp
+++ src/interface/Interface.cpp
@@ -9,22 +9,24 @@
 #include <algorithm>
 #include "../modules/renderer/RendererModule.h"
 #include "../modules/transform/TransformModule.h"
+#include <limits>
 
 Interface::Interface(std::vector<std::unique_ptr<Unit>>& initSceneUnits)
     : sceneUnits(initSceneUnits) {}
 
 void Interface::introduction() const
 {
-    std::cout << "Welcome to MiniECS by Raoul Kaleba\n";
+    std::cout << "Welcome to MiniECS v0.11 by Raoul Kaleba\n";
 }
 
 void Interface::displayCommands() const
 {
-    std::cout << "Your avaliable commands are\n"
-    << "create_unit\n"
-    << "add_module\n"
-    << "remove_module\n"
-    << "play\n";
+    std::cout << "Your available commands are:\n"
+    << " create_unit -> Create a new Unit with a name\n"
+    << " add_module -> Add a Module to a specific Unit by name\n"
+    << " remove_module -> Remove a Module from a specific Unit by name\n"
+    << " play -> Run all Units' modules' start functions\n"
+    << " exit -> Exit MiniECS\n";
 }
 
 void Interface::createUnit()
@@ -46,7 +48,6 @@ void Interface::addModule()
     std::cout << "Module Type (1. Renderer, 2. Transform): ";
     std::cin >> moduleType;
 
-    // TODO: Fix cannot find bug after creating or failing to create
     for (auto& unit : sceneUnits)
     {
         if (unit->getName() == unitName)
@@ -56,19 +57,23 @@ void Interface::addModule()
             {
             case 1:
                 unit->addModule(std::make_unique<RendererModule>("Renderer"));
+                std::cin.ignore();
                 break;;
             case 2:
                 unit->addModule(std::make_unique<TransformModule>("Transform"));
+                std::cin.ignore();
                 break;
             default:
                 std::cout << "Invalid Module Type!! \n";
+                std::cin.ignore();
                 break;
             }
         }
     }
     if (!found)
     {
-        std::cout << "Could not find: " << unitName << "\n";
+        std::cout << "Could not find Unit: " << unitName << "\n";
+        std::cin.ignore();
     }
 
 }
@@ -77,19 +82,21 @@ void Interface::removeModule()
 {
     bool unitFound = false;
     bool moduleFound = false;
+    bool moduleStartFind = false;
     std::string unitName;
     int moduleID;
     std::cout << "Unit Name: ";
     std::getline(std::cin, unitName);
-    std::cout << "Module ID: ";
-    std::cin >> moduleID;
 
-    // TODO: Fix cannot find bug after creating or failing to create
     for (auto& unit : sceneUnits)
     {
         if (unit->getName() == unitName)
         {
             unitFound = true;
+            std::cout << "Module ID: ";
+            std::cin >> moduleID;
+            std::cin.ignore();
+            moduleStartFind = true;
             for (auto& module : unit->getModulesVector())
             {
                 std::vector<std::unique_ptr<Module>>& modulesVec = unit->getModulesVector();
@@ -105,7 +112,7 @@ void Interface::removeModule()
     {
         std::cout << "Could not find: " << unitName << "\n";
     }
-    if (!moduleFound)
+    if (!moduleFound && moduleStartFind)
     {
         std::cout << "Could not find module in: " << unitName << " with ID: " << moduleID << "\n";
     }
@@ -114,28 +121,51 @@ void Interface::removeModule()
 
 void Interface::play()
 {
-    for (auto& unit : sceneUnits)
+    if (!sceneUnits.empty())
     {
-        unit->callModuleStart();
+        for (auto& unit : sceneUnits)
+        {
+            if (!unit->getModulesVector().empty())
+            {
+                unit->callModuleStart();
+            }
+            else
+            {
+                std::cout << "No units in the scene have modules\n";
+            }
+        }
+    }
+    else
+    {
+        std::cout << "No units in the scene\n";
     }
 }
 
 void Interface::handleInput()
 {
     std::string command;
-    std::cout << "Enter command: ";
+    std::cout << "MiniECS/v0.11> ";
     std::getline(std::cin, command);
 
     if (command == "create_unit") {
         createUnit();
-    } else if (command == "add_module") {
+    }
+    else if (command == "add_module") {
         addModule();
-    } else if (command == "remove_module")
+    }
+    else if (command == "remove_module")
     {
         removeModule();
-    } else if (command == "play") {
+    }
+    else if (command == "play") {
         play();
-    } else {
-        std::cout << "Unknown command.\n";
+    }
+    else if (command == "exit") {
+        std::cout << "Exiting MiniECS v0.11\n";
+        return exit(0);
+    }
+    else {
+        std::cout << "Unknown command\n";
+        displayCommands();
     }
 }
